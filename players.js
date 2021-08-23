48dc9cb1f8b1f909e8bf2ced094e30529cff86c2
md ezaz <mdezaz@mds-MacBook-Pro.local>
2021-08-23 15:35:47 +0600

    from mac2

--- players.js
+++ players.js
@@ -1,6 +1,31 @@
+function setPlayerStyle(player) {
+    player.style.border = '1px solid red';
+    player.style.borderRadius = '15px';
+
+}
 const players = document.getElementsByClassName('player');
 for (const player of players) {
+    setPlayerStyle(player);
+    player.addEventListener('click', function () {
+        player.style.backgroundColor = 'red';
+    })
+
+
 
 
 
 }
+
+function addPlayer() {
+    const playersContainer = document.getElementById("players");
+    const player = document.createElement('div');
+    player.classList.add('player');
+
+
+}
+
+document.getElementById('players').addEventListener('click', function (event) {
+
+
+
+})
